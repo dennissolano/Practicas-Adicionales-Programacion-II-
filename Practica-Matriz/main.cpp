e554ff417a321eeaa2fdb97db39696e3b4c179da
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2017-10-09 20:13:10 -0600

    Pruebas en main....

--- Practica-Matriz/main.cpp
+++ Practica-Matriz/main.cpp
@@ -3,11 +3,16 @@
  * Author: alan.calderon
  */
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+
 #include "Matriz.h"
 
 int main(int argc, char** argv)
 {
+    srand( time( NULL ) );
+
     Matriz cinco; // Crea una matriz de 5x5.
     Matriz nueve( 9, 9 ); // Crea una matriz de 9x9.
     Matriz cincoBis;
@@ -16,12 +21,38 @@ int main(int argc, char** argv)
     // Llena las matrices con datos válidos:
     // prográmelo usted...
 
-    // Realiza operaciones con las matrices semejantes:
-    Matriz cincoSuma = cinco + cincoBis;
-    // programe otras operaciones...
+    for( int indiceFila = 0; indiceFila < 5; ++indiceFila )
+    {
+        for( int indiceColumna = 0; indiceColumna < 5; ++indiceColumna )
+        {
+            cinco.asgValor( indiceFila, indiceColumna, rand( ) % 10 );
+            cincoBis.asgValor( indiceFila, indiceColumna, rand( ) % 10 );
+        }
+    }
 
-    // Imprime resultados:
-    // prográmelo usted...
+    std::cout << "Matriz A" << std::endl << std::endl;
+    cinco.imprimir( );
+
+    std::cout << "Matriz B" << std::endl << std::endl;
+    cincoBis.imprimir( );
+
+
+    // Realiza operaciones con las matrices semejantes:
+
+    std::cout << "Producto de matrices: AXB" << std::endl << std::endl;
+    Matriz cincoProducto = cinco * cincoBis;
+    cincoProducto.imprimir( );
+
+    std::cout << "Suma de matrices: A+B" << std::endl << std::endl;
+    Matriz cincoSuma = cinco + cincoBis;
+    cincoSuma.imprimir( );
+
+    std::cout << "Resta de matrices: A-B" << std::endl << std::endl;
+    Matriz cincoResta = cinco - cincoBis;
+    cincoResta.imprimir( );
+
+    // Programe otras operaciones...
+
     return 0;
 }
 
