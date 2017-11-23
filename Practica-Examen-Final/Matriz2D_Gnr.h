04f7afd1fefc35629719b701091c618a2359c17b
guest-iys7rn <guest-iys7rn@localhost>
2017-11-23 05:38:24 -0600

    Inicializacion de matriz con numeros aleatorios entre 0-10 Ademas de pequeno inconveniente en obtValor que estaba retornando la direccion de memoria.

--- Practica-Examen-Final/Matriz2D_Gnr.h
+++ Practica-Examen-Final/Matriz2D_Gnr.h
@@ -1,14 +1,15 @@
 #ifndef MATRIZ2D_GNR_H
 #define MATRIZ2D_GNR_H
 
-#include <iostream>
+#include <cstdlib>
 #include <iomanip>
+#include <iostream>
 #include <memory>
 #include <vector>
 
 using namespace std;
 
-template< typename V > // V: tipod de dato.
+template< typename V > // V: tipo de de dato.
 class Matriz2D_Gnr
 {
 private:
@@ -22,111 +23,120 @@ public:
     /* Constructores */
 
     // Constructor estándar.
-    Matriz2D_Gnr( );
+    Matriz2D_Gnr();
 
     // Constructor de copias.
-    Matriz2D_Gnr( const Matriz2D_Gnr< V >& origen );
+    Matriz2D_Gnr(const Matriz2D_Gnr< V >& origen);
 
     // Construye una matriz con m filas y n columnas. Inicializada con ceros.
-    Matriz2D_Gnr( int m, int n );
+    Matriz2D_Gnr(int m, int n);
 
     // Destructor.
-    ~Matriz2D_Gnr( );
+    ~Matriz2D_Gnr();
 
     /* Métodos Obtenedores */
 
     // REQ: *this haya sido correctamente inicializada.
     // EFE: retorna la cantidad de filas en *this.
-    int obtCantidadFilas( ) const;
+    int obtCantidadFilas() const;
 
     // REQ: *this haya sido correctamente inicializada.
     // EFE: retorna la cantidad de columnas en *this.
-    int obtCantidadColumnas( ) const;
+    int obtCantidadColumnas() const;
 
     // REQ: 0 <= fila < cantidadFilas / 0 <= columna < cantidadColumnas.
     // EFE: retorna el elemento en la posición (fila, columna) de *this.
-    V obtValor( int fila, int columna ) const;
+    V obtValor(int fila, int columna) const;
 
     // REQ: *this haya sido correctamente inicializada.
     // EFE: retorna la matriz transpuesta de *this.
-    Matriz2D_Gnr< V >& transpuesta( ) const;
+    Matriz2D_Gnr< V >& transpuesta() const;
 
     /* Métodos Modificadores */
 
     // REQ: 0 <= fila < cantidadFilas / 0 <= columna < cantidadColumnas.
     // EFE: inserta en la posición (fila, columna) el valor v.
     // MOD: el valor del elemento contenido en la posición (fila, columna) de *this.
-    void asgValor( int fila, int columna, V valor );
+    void asgValor(int fila, int columna, V valor);
 
     /* Operadores */
 
-    Matriz2D_Gnr<V>& operator=( const Matriz2D_Gnr< V >& origen );
-    Matriz2D_Gnr< V >& operator+( const Matriz2D_Gnr< V >& origen ) const;
-    Matriz2D_Gnr< V >& operator*( const Matriz2D_Gnr< V >& origen ) const;
-    Matriz2D_Gnr< V >& operator%( int escalar ) const;
+    Matriz2D_Gnr<V>& operator=(const Matriz2D_Gnr< V >& origen);
+    Matriz2D_Gnr< V >& operator+(const Matriz2D_Gnr< V >& origen) const;
+    Matriz2D_Gnr< V >& operator*(const Matriz2D_Gnr< V >& origen) const;
+    Matriz2D_Gnr< V >& operator%(int escalar) const;
 
     /* Otros */
 
     // REQ: *this haya sido correctamente inicializada.
     // EFE: imprime el contenido de *this.
-    void imprimirMatriz( )const;
+    void imprimirMatriz()const;
 };
 
 template< typename V >
-Matriz2D_Gnr< V >::Matriz2D_Gnr( int m, int n )
-: cantidadFilas( m )
-, cantidadColumnas( n )
+Matriz2D_Gnr< V >::Matriz2D_Gnr(int m, int n)
+: cantidadFilas(m)
+, cantidadColumnas(n)
 {
-    matriz.resize( m*n, V( ) );
+    matriz.resize(m*n, V());
+
+    // Inicializar la matriz con numeros aleatorios entre 0 - 10
+    srand(time(NULL));
+    for (int i = 0; i < cantidadFilas; ++i)
+    {
+        for (int j = 0; j < cantidadColumnas; ++j)
+            asgValor(i, j, rand() % 11);
+    }
 }
 
 template< typename V >
-Matriz2D_Gnr< V >::Matriz2D_Gnr( const Matriz2D_Gnr<V>& origen )
-: cantidadFilas( origen.cantidadFilas )
-, cantidadColumnas( origen.cantidadColumnas )
-, matriz( origen.matriz )
+Matriz2D_Gnr< V >::Matriz2D_Gnr(const Matriz2D_Gnr<V>& origen)
+: cantidadFilas(origen.cantidadFilas)
+, cantidadColumnas(origen.cantidadColumnas)
+, matriz(origen.matriz)
 {
 }
 
 template< typename V >
-Matriz2D_Gnr< V >::~Matriz2D_Gnr( )
+Matriz2D_Gnr< V >::~Matriz2D_Gnr()
 {
 }
 
 template< typename V >
-int Matriz2D_Gnr< V >::obtCantidadFilas( ) const
+int Matriz2D_Gnr< V >::obtCantidadFilas() const
 {
     return cantidadFilas;
 }
 
 template< typename V >
-int Matriz2D_Gnr< V >::obtCantidadColumnas( ) const
+int Matriz2D_Gnr< V >::obtCantidadColumnas() const
 {
     return cantidadColumnas;
 }
 
 template< typename V >
-V Matriz2D_Gnr< V >::obtValor( int fila, int columna ) const
+V Matriz2D_Gnr< V >::obtValor(int fila, int columna) const
 {
-    matriz[fila * cantidadColumnas + columna ];
+    V valor = matriz[fila * cantidadColumnas + columna ];
+    return valor;
 }
 
 template< typename V >
-Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::transpuesta( ) const
+Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::transpuesta() const
 {
 }
 
 template< typename V >
-void Matriz2D_Gnr< V >::asgValor( int fila, int columna, V valor )
+void Matriz2D_Gnr< V >::asgValor(int fila, int columna, V valor)
 {
     matriz[fila * cantidadColumnas + columna ] = valor;
 }
 
 template< typename V >
-Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator=( const Matriz2D_Gnr<V>& origen )
+Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator=(const Matriz2D_Gnr<V>& origen)
 {
     // Evita que *this se copie a sí mismo.
-    if ( this != &origen )
+    if (this != &origen)
     {
 
         cantidadFilas = origen.cantidadFilas;
@@ -137,17 +147,17 @@ Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator=( const Matriz2D_Gnr<V>& origen )
 }
 
 template< typename V >
-Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator+( const Matriz2D_Gnr< V >& origen ) const
+Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator+(const Matriz2D_Gnr< V >& origen) const
 {
-    Matriz2D_Gnr< V >* matrizSuma = new Matriz2D_Gnr( cantidadFilas, cantidadColumnas );
+    Matriz2D_Gnr< V >* matrizSuma = new Matriz2D_Gnr(cantidadFilas, cantidadColumnas);
 
-    for ( int fila = 0; fila < cantidadFilas; ++fila )
+    for (int fila = 0; fila < cantidadFilas; ++fila)
     {
-        for ( int columna = 0; columna < cantidadColumnas; ++columna )
+        for (int columna = 0; columna < cantidadColumnas; ++columna)
         {
 
-            V valor = this->obtValor( fila, columna ) + origen.obtValor( fila, columna );
-            matrizSuma->asgValor( fila, columna, valor );
+            V valor = this->obtValor(fila, columna) + origen.obtValor(fila, columna);
+            matrizSuma->asgValor(fila, columna, valor);
         }
     }
 
@@ -155,23 +165,23 @@ Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator+( const Matriz2D_Gnr< V >& origen
 }
 
 template< typename V >
-Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator*( const Matriz2D_Gnr< V >& origen ) const
+Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator*(const Matriz2D_Gnr< V >& origen) const
 {
 }
 
 template< typename V >
-Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator%( int escalar ) const
+Matriz2D_Gnr< V >& Matriz2D_Gnr< V >::operator%(int escalar) const
 {
 }
 
 template< typename V >
-void Matriz2D_Gnr< V >::imprimirMatriz( ) const
+void Matriz2D_Gnr< V >::imprimirMatriz() const
 {
-    for ( int i = 0; i < cantidadFilas; ++i )
+    for (int i = 0; i < cantidadFilas; ++i)
     {
-        for ( int j = 0; j < cantidadColumnas; ++j )
+        for (int j = 0; j < cantidadColumnas; ++j)
         {
-            cout << setw( 10 ) << left << obtValor( i, j );
+            cout << setw(10) << left << obtValor(i, j);
         }
         cout << endl;
     }
